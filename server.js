f9a67685de981304d47d91982d2c8598a137d307
martingtrrz <martin.050913gtrrz@gmail.com>
2025-04-04 09:24:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY || fa98cab2aaa84b4ab5a161835250703;
 
 app.use(cors());
 app.use(express.static("public")); // Servir archivos estáticos
